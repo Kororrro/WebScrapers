57fb634d0da69f8fab86f8beaaca14e4d94b048a
Kororrro <korofm40@gmail.com>
2026-03-27 23:35:45 +0100

    test1 - added more controll over what is executed, bypass reddit's errors

--- scrapers.py
+++ scrapers.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-def initialize():
+def initialize(args=[]):
     options = webdriver.FirefoxOptions()
+    for el in args:
+        options.add_argument(f"--{el}")
+        # print(f"--{el}")
     service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(service=service)
+    driver = webdriver.Firefox(options=options,service=service)
     return driver
+
+def findAndClick(site,arg,tag):
+    elems = site.find_elements(getattr(By, arg), f"{tag}")
+    print(elems)
+    for el in range(len(elems)):
+        try:
+            elems[el].click()
+            print("element clicked")
+        except:
+            continue
+    
--- test1.py
+++ test1.py
@@ -2,60 +2,50 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+import scrapers
 import time
 
 """
 Write to file a list of posts on a reddit subreddit
 """
-
+DoWrite = 0
+DoPrint = 1
 element_list = []
 
-try:
-    f = open("./links.txt", "w")
-except:
-    f = open("./links.txt", "x")
+driver = scrapers.initialize(["no-sandbox","disable-dev-shm-usage"])
 
-# Set up Chrome options (optional)
-options = webdriver.FirefoxOptions()
-options.add_argument("--headless")  # Run in headless mode (optional)
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-# Use a proper Service object
-service = Service(GeckoDriverManager().install())
-
-# for page in range(1, 5):
-    # Initialize driver properly
-driver = webdriver.Firefox(options=options, service=service)
-
-    # Load the URL
 url = f"https://www.reddit.com/r/webscraping/"
 driver.get(url)
 time.sleep(2)  # Optional wait to ensure page loads
+driver.refresh()
 
 # Extract product details
 titles = driver.find_elements(By.CLASS_NAME, "absolute")
-# prices = driver.find_elements(By.CLASS_NAME, "price")
-# descriptions = driver.find_elements(By.CLASS_NAME, "description")
-# ratings = driver.find_elements(By.CLASS_NAME, "ratings")
 
 # Store results in a list
 for i in range(len(titles)):
     if titles[i].text != '':
+        print("getting elements")
         element_list.append([
             titles[i].text,
             titles[i].get_attribute("href")
-        # prices[i].text,
-        # descriptions[i].text,
-        # ratings[i].text
         ])
-
 # element_list.remove('')
-driver.quit()
+# driver.quit()
 
-# Display extracted data
-for row in element_list:
-    for i in row:
-        f.write(i + ": ")
-    f.write("\n")
+if(DoWrite == 1):
+    try:
+        f = open("./links.txt", "w")
+    except:
+        f = open("./links.txt", "x")
+    # Write extracted data
+    for row in element_list:
+        for i in row:
+            print("%s: ", i)
+            f.write(i + ": ")
+        f.write("\n")
 
+if(DoPrint == 1):
+    for row in element_list:
+        for i in row:
+            print("%s: " % i)
